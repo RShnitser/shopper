e26e53dbca6cd7e0e25d702545413ec5b69f0223
Roman Shnitser <romanshnitser@hotmail.com>
2022-09-25 17:49:04 -0400

    Add commercejs api

--- src/components/PageHome/PageHome.jsx
+++ src/components/PageHome/PageHome.jsx
@@ -1,11 +1,50 @@
 import React from "react";
+import { commerce } from "../../services";
 
 class PageHome extends React.Component {
 
+    state = {
+        loading: true,
+        products: []
+    }
+
+    fetchProducts = async () => {
+        
+        const {data} = await commerce.products.list();
+
+        this.setState({
+            products: data,
+            loading: false
+        });
+    }
+
+    componentDidMount() {
+        this.fetchProducts();
+    }
+
     render() {
 
+        const{ loading, products} = this.state;
+
+        let display;
+
+        if(loading) {
+            display = <div>Loading...</div>;
+        }
+        else {
+            display = products && products.map(function(product) {
+                return <div key={product.name}>{product.name}</div>;
+            });
+        }
+
         return(
-            <div>Home</div>
+            <div>
+                <div>
+                    <div>Shopper</div>
+                    <div><i className="fa-solid fa-cart-shopping"></i></div>
+                </div>
+                {display}
+            </div>
         );
     }
 }
