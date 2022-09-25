c78a3cac04029b2380464152c18093caec762b1c
Roman Shnitser <romanshnitser@hotmail.com>
2022-09-24 22:00:16 -0400

    Add home page

--- src/components/ShopperApp/ShopperApp.jsx
+++ src/components/ShopperApp/ShopperApp.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { APP_PAGE } from "../constants";
+import PageHome from "../PageHome/PageHome";
 
 class ShopperApp extends React.Component {
 
+    state = {
+        page: APP_PAGE.PAGE_HOME,
+    }
+
     render() {
 
-        return(
-            <div></div>
-        )
+        const {page} = this.state;
+
+        let currentPage = null;
+
+        switch(page) {
+            case APP_PAGE.PAGE_HOME:
+                currentPage = <PageHome />
+            break;
+            default:
+            break;
+        }
+
+        return(currentPage);
     }
 }
 
---
+++ src/components/constants.js
@@ -0,0 +1,9 @@
+export const APP_PAGE = {
+    PAGE_HOME: "h",
+    PAGE_LOGIN: "l",
+    PAGE_CREATE: "c",
+    PAGE_CART: "i",
+    PAGE_SHIPPING: "s",
+    PAGE_PAYMENT: "p",
+    PAGE_CONFIRMATION: "f",
+}
